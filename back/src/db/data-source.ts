7128596cedea7b09cf8a140ee9e54792835880e7
nilsriga <nils.riga@gmail.com>
2024-10-26 19:45:35 +0300

    testing actions

--- back/src/db/data-source.ts
+++ back/src/db/data-source.ts
@@ -11,9 +11,9 @@ export const AppDataSource = new DataSource({
   type: 'mysql',
   host: configService.get<string>('DATABASE_HOST', 'localhost'), // Default to 'localhost' if not set
   port: configService.get<number>('DATABASE_PORT', 3306), // Default to 3306 if not set
-  username: configService.get<string>('DATABASE_USERNAME', 'root'), // Default username
-  password: configService.get<string>('DATABASE_PASSWORD', ''), // Default to an empty password
-  database: configService.get<string>('DATABASE_NAME', 'test'), // Default database name
+  username: configService.get<string>('DATABASE_USERNAME', 'sammy'), // Default username
+  password: configService.get<string>('DATABASE_PASSWORD', 'asdf'), // Default to an empty password
+  database: configService.get<string>('DATABASE_NAME', 'currencies'), // Default database name
   entities: [`${__dirname}/../db/entities/*.entity.{js,ts}`],
   migrations: [`${__dirname}/../db/migrations/*.{js,ts}`],  
   synchronize: true, // Use 'true' only in development
